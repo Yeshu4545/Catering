1cd7775bbc5aed79b66c2c3adc40d7820688fba9
Yashwanth Uppula <84965878+Yeshu4545@users.noreply.github.com>
2025-05-10 16:53:51 +0530

    Update Api.jsx

--- sattva/src/components/Api/Api.jsx
+++ sattva/src/components/Api/Api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/checkout';
+const API_URL = 'https://catering-47l4.onrender.com/api/checkout';
 
 export const placeOrder = async (orderData) => {
     try {
